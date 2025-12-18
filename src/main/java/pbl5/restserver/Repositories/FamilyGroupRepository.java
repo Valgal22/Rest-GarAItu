40aa7399d1a2bf2f34036c9d2e24788cd8b172f8
Josu <josu.agirre2@gmail.com>
2025-12-18 12:02:27 +0100

    Dev branch

--- src/main/java/pbl5/restserver/Repositories/FamilyGroupRepository.java
+++ src/main/java/pbl5/restserver/Repositories/FamilyGroupRepository.java
@@ -4,8 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import pbl5.restserver.model.FamilyGroup;
 
-import java.util.List;
-
 public interface FamilyGroupRepository extends JpaRepository<FamilyGroup, Long> {
     
 }
